9ab02d8353da6005fa70dfa703ba1b5cff66c9d7
Pers test <qa61test@gmail.com>
2023-05-31 00:52:35 +0400

    add Tests

--- src/test/java/netology/TodosTest.java
+++ src/test/java/netology/TodosTest.java
@@ -74,4 +74,73 @@ public class TodosTest {
         Task[] actual = todos.search("Приложение НетоБанка");
         Assertions.assertArrayEquals(expected, actual);
     }
+
+    @Test
+    public void shouldFindSeveralTask() {
+        SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(13, "Позвонить родителям");
+        SimpleTask simpleTask3 = new SimpleTask(666, "Позвонить родителям");
+        Meeting meeting = new Meeting(
+                888,
+                "Позвонить родителям",
+                "Семейный очаг",
+                "Внезамедлительно"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(meeting);
+
+        Task[] expected = {simpleTask1, simpleTask2, simpleTask3, meeting};
+        Task[] actual = todos.search("Позвонить родителям");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindOneTask() {
+        SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(13, "Позвонить родителям");
+        SimpleTask simpleTask3 = new SimpleTask(666, "Позвонить родителям");
+        Meeting meeting = new Meeting(
+                888,
+                "Позвонить родителям",
+                "Семейный очаг",
+                "Внезамедлительно"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+        todos.add(meeting);
+
+        Task[] expected = {meeting};
+        Task[] actual = todos.search("Семейный очаг");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void noTasksFound() {
+        SimpleTask simpleTask1 = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(13, "Сходить в спорт. зал");
+        Meeting meeting = new Meeting(
+                888,
+                "Собрать мотор",
+                "Мотоцикл мечты",
+                "В понедельник утром"
+        );
+
+        Todos todos = new Todos();
+
+        todos.add(simpleTask1);
+        todos.add(simpleTask2);
+        todos.add(meeting);
+
+        Task[] expected = {};
+        Task[] actual = todos.search("Перебрать КПП");
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
